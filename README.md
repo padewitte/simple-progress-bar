f009a8d9a32a4f2bd30a242cb246a80785d91c1b
DEWITTE Pierre-Alban <pierre-alban.dewitte@myscript.com>
2017-04-22 15:08:37 +0200

    Initial import

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # simple-progress-bar
 A simple progress bar to demo the beauty of custom elements
+
+## What
+This custom element demo all the beahviors expected from a custom element :
+
+- Attribut definition : MAX Limit
+- Attribut modification : Update MAX limit
+- Two way binding of element : Nb of active elements 
+- Listen to events inside the component : Click change the color
+- Listen to events emit by the CE : Fire an event when nb of active element > Limit
+- Change style of the CE : Change with of the bar
+- Call a method: Increase limit
+
+TODO
+
+## Run
+No build, just launch a local server.
+`python -m SimpleHTTPServer`
---
+++ simple-progress-bar.js
@@ -0,0 +1,200 @@
+/**
+ * A progress bar implementation with Custom Element V1.
+ *
+ */
+
+function convertToNumberIfPossible(value) {
+  if (Number.isNaN(parseInt(value))) {
+    return value;
+  } else {
+    return parseInt(value);
+  }
+}
+
+function camelCase(input) {
+  return input.toLowerCase().replace(/-(.)/g, function(match, group1) {
+    return group1.toUpperCase();
+  });
+}
+
+class ProgressBarElement extends HTMLElement {
+
+  constructor(self) {
+    console.log('Constructing a new progress bar');
+    self = super(self);
+    // click event does not catch a middle button click in firefox
+    self.addEventListener('mouseup', e => {
+      console.log(e);
+      // If left button we increase the limit, otherwise we decrease it.
+      if (e.button === 0) {
+        self.increaseLimit();
+      } else {
+        self.decreaseLimit();
+      }
+      e.preventDefault();
+      return false;
+    });
+    self.connected = false;
+    return self;
+  }
+
+  // Tells the element which attributes to observer for changes
+  // This is a feature added by Custom Elements
+  static get observedAttributes() {
+    return ['max', 'current'];
+  }
+
+  // Get the initial value of foo or use a fallback
+  connectedCallback() {
+    console.log('Connected callback');
+    this.max = this.getAttribute('max') || 3;
+    this.current = this.getAttribute('current') || 0;
+    this.connected = true;
+    this.render();
+  }
+
+  disconnectedCallback() {
+    // Called every time the element is removed from the DOM.
+    console.log('nothing currenlty done at deconnexion');
+  }
+
+  adoptedCallback() {
+    console.log('nothing currently done at adoption');
+    // Called if the element has been moved into a new document
+  }
+
+  // When an attribute is updated, check if the value is different
+  // If so, call our setter
+  attributeChangedCallback(name, oldVal, newVal) {
+    console.log('Attribute change callback', name, newVal, 'was', oldVal);
+    if (this[name] !== newVal) {
+      this[name] = newVal;
+      this.updateRendering();
+      this.dispatchEvent(new CustomEvent(name + "Change", { detail: { value: newVal } }));
+    }
+  }
+
+  updateAttribute(att, value, defaultValue) {
+    console.log('Updating attribute', att, value);
+    if (value !== undefined && value !== null) {
+      const assignedValue = convertToNumberIfPossible(value);
+      this['_' + camelCase(att)] = assignedValue;
+      this.setAttribute(att, assignedValue);
+    } else {
+      const assignedValue = convertToNumberIfPossible(defaultValue);
+      this['_' + camelCase(att)] = assignedValue;
+      this.setAttribute(att, assignedValue);
+    }
+  }
+
+  get max() {
+    return this._max;
+  }
+
+  set max(value) {
+    this.updateAttribute('max', value, 3);
+
+  }
+
+  get current() {
+    return this._current;
+  }
+
+  set current(value) {
+    this.updateAttribute('current', value, 0);
+  }
+
+  getRatio() {
+    if (this._current === 0) {
+      return 0;
+    }
+    return Math.min(100, (parseInt(this._current) * 100) / parseInt(this._max));
+  }
+
+  updateRendering() {
+    console.log('Updating rendering');
+    if (this.connected === true) {
+      this.querySelector('#barStatus').style.width = this.getRatio() + '%';
+      this.querySelector('#legenda').innerText = this._current + '/' + this._max
+      if (this.getRatio() >= 100) {
+        this.dispatchEvent(new CustomEvent("limit-reach", {
+          detail: {
+            txt: 'Limit reach',
+            current: this.current,
+            max: this.max
+          }
+        }));
+      } else {
+        this.dispatchEvent(new CustomEvent("under-limit", {
+          detail: {
+            txt: 'Under limit',
+            current: this.current,
+            max: this.max
+          }
+        }));
+      }
+    }
+  }
+
+  render() {
+    console.log('Rendering the custome element');
+    this.innerHTML = `
+      <style>
+        #progressBar {
+            position: relative;
+            width: 100%;
+            height: 30px;
+            background-color: rgb(100, 108, 119);
+            /*text-align: center;*/
+            display: flex;
+            align-items: center;
+            z-index: 1;
+        }
+
+        #barStatus {
+            position: absolute;
+            width: 1px;
+            left: 0;
+            top: 0;
+            height: 100%;
+            background-color: rgba(0, 0, 255, 0.5);
+            transition: width 2s;
+            z-index: 4;
+        }
+        
+        #legenda {
+            color: #ffffff;
+            padding: 5px;
+            margin: auto;
+            z-index: 5;
+        }
+      </style>
+        <div id="progressBar">
+            <div id="legenda">${this._current}/${this._max}</div>
+            <div id="barStatus"></div>
+            
+      </div>`;
+    window.setTimeout(() => {
+      this.updateRendering();
+    }, 10);
+    //<div id="barStatusLeft">/${this._current}</div>
+  }
+
+  increaseLimit() {
+    this.max = parseInt(this.max) + 1;
+  }
+
+  decreaseLimit() {
+    if (this.max > 0) {
+      this.max = parseInt(this.max) - 1;
+    }
+  }
+
+}
+
+if (!window.customElements.get('custom-progress-bar')) {
+  window.customElements.define('custom-progress-bar', ProgressBarElement);
+}
+
+
+export default ProgressBarElement
